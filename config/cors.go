7e47c8fbd1192c63704e7bb61732591f76429c55
serlip06 <114634943+serlip06@users.noreply.github.com>
2024-05-09 17:56:08 +0700

    semoga nyambung ke fe

--- config/cors.go
+++ config/cors.go
@@ -17,9 +17,9 @@ var origins = []string{
 	"https://iteung.ulbi.ac.id",
 	"https://whatsauth.github.io",
 	"https://127.0.0.1.:5500",
-	"http://127.0.0.1:8080",
 	"http://127.0.0.1:40013",
 	"https://serlip06.github.io",
+	"http://127.0.0.1:8080",
 }
 
 var Internalhost string = os.Getenv("INTERNALHOST") + ":" + os.Getenv("PORT")
