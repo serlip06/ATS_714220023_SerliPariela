9366f8ae5e3979967865d6bffcab59067c1fd827
serlip06 <114634943+serlip06@users.noreply.github.com>
2024-05-09 19:07:10 +0700

    push!!

--- config/cors.go
+++ config/cors.go
@@ -17,7 +17,7 @@ var origins = []string{
 	"https://iteung.ulbi.ac.id",
 	"https://whatsauth.github.io",
 	"https://127.0.0.1.:5500",
-	//"http://127.0.0.1:40013",
+	"http://127.0.0.1:40013",
 	"https://serlip06.github.io",
 	"http://127.0.0.1:8080",
 }
