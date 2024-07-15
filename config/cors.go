59e8d027eed75675b6362d2eb44d0749b3438abf
serlip06 <114634943+serlip06@users.noreply.github.com>
2024-07-15 14:40:02 +0700

    berhasilinimah

--- config/cors.go
+++ config/cors.go
@@ -28,7 +28,7 @@ var Internalhost string = os.Getenv("INTERNALHOST") + ":" + os.Getenv("PORT")
 
 var Cors = cors.Config{
 	AllowOrigins:     strings.Join(origins[:], ","),
-	AllowMethods: "GET,HEAD,OPTIONS,POST,PUT,DETELE",
+	AllowMethods: 	  "GET, HEAD, OPTIONS, POST, PUT, DETELE",
 	AllowHeaders:     "Origin,Login, Content-Type",
 	ExposeHeaders:    "Content-Length",
 	AllowCredentials: true,
